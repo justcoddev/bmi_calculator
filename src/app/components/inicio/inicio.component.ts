1e6fbbca194145ca1ae8c70f3b56504bd3ba0d51
justcoddev <edisonpaulcrz@gmail.com>
2022-11-02 00:00:08 -0500

    ngClass

--- src/app/components/inicio/inicio.component.ts
+++ src/app/components/inicio/inicio.component.ts
@@ -9,9 +9,21 @@ export class InicioComponent implements OnInit {
 
   edad = 25;
   peso = 60;
+  altura = 170;
+  sexo = 'Masculino';
+
   constructor() { }
 
   ngOnInit(): void {
   }
+  cambiarAltura(event: any) {
+    this.altura = event.target.value;
+  }
 
+  masculino() {
+    this.sexo = 'Masculino';
+  }
+  femenino() {
+    this.sexo = 'Femenino';
+  }
 }
